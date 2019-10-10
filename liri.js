28edb2fe041a252b406651229ed756c9e5bb4f50
StephenTHendrix <stephenthendrix@gmail.com>
2019-10-10 13:25:49 -0500

    order

--- liri.js
+++ liri.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 
-Spotify = require("node-spotify-api");
-
 var keys = require("./keys.js");
 
+Spotify = require("node-spotify-api");
+
 var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+var moment = require("moment");
+
+var command = process.argv[2];
+
+
+
